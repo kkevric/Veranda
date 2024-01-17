f3fea750e006d75b15604192c94d3be90887d5ff
Katja Kevric <katja.kevric@gmail.com>
2024-01-17 20:13:11 +0100

    fixing element aligements

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
+
 const pathName = window.location.pathname;
 const pageName = pathName.split("/").pop();
 
-if(pageName === "index.html"){
+if(pageName === "menu.html"){
     document.querySelector(".home").classList.add("activeLink");
 }
-if(pageName === "menu.html"){
-    document.querySelector(".menu").classList.add("activeLink");
+if(pageName === "vege.html"){
+    document.querySelector(".katjak").classList.add("activeLink");
 }
-if(pageName === "contact.html"){
-    document.querySelector(".contact").classList.add("activeLink");
+if(pageName === "starters.html"){
+    document.querySelector(".katjake").classList.add("activeLink");
 }
+
+const navLinkEls =document.querySelectorAll(".ketikaproba");
+const windowPathname = window.location.pathname;
+
+navLinkEls.forEach(navLinkEls=>
+    {
+        if(navLinkEl.href.includes(windowPathname)){
+            navLinkEl.classList.add("active");
+        }
+
+    });
